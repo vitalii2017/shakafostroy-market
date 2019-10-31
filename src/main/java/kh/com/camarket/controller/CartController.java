13f5eec59298380b45bb008e4d9b4bfc3aea646f
Vitalii Shevchuk <svs_2008@ukr.net>
2019-10-31 14:11:04 +0300

    initial commit

---
+++ src/main/java/kh/com/camarket/controller/CartController.java
@@ -0,0 +1,43 @@
+package kh.com.camarket.controller;
+
+import kh.com.camarket.model.Customer;
+import kh.com.camarket.service.CartService;
+import kh.com.camarket.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Yuth on 3/28/2017.
+ */
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @RequestMapping
+    public String get(@AuthenticationPrincipal User activeUser) {
+        Customer customer = customerService.getCustomerByUsername(activeUser.getUsername());
+        int cartId = customer.getCart().getCartId();
+        return "redirect:/cart/"+cartId;
+    }
+
+    @RequestMapping(value="/{cartId}", method = RequestMethod.GET)
+    public String getCart(@PathVariable(value = "cartId") int cartId, Model model) {
+        model.addAttribute("cartId", cartId);
+
+        return "cart";
+    }
+
+}
